38152834273e7e439daf7fd4778cabeb89740d7e
younglove321 <younglove321@naver.com>
2019-09-21 22:03:18 +0900

    find id

--- app/src/main/java/com/e/findid/MainActivity.java
+++ app/src/main/java/com/e/findid/MainActivity.java
@@ -2,11 +2,16 @@ package com.e.findid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.AdapterView;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.view.View.OnClickListener;
+
+
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -15,6 +20,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     Spinner spinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,9 +31,9 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); //뒤로가기
 
         //spinner부분 (국가번호)
-        final String[] data= getResources().getStringArray(R.array.nation);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,data);
-        Spinner spinner = (Spinner)findViewById(R.id.spinner);
+        final String[] data = getResources().getStringArray(R.array.nation);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, data);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
         spinner.setAdapter(adapter);
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -40,12 +46,24 @@ public class MainActivity extends AppCompatActivity {
             public void onNothingSelected(AdapterView<?> parent) {
 
             }
+
+
         });
 
+        Button submit = (Button) findViewById(R.id.submit_button);
+        submit.setClickable(true);
+        submit.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "아이디 찾기 완료", Toast.LENGTH_LONG).show();
 
+                // 액티비티 전환 코드
+                Intent intent = new Intent(getApplicationContext(), result.class);
+                startActivity(intent);
+            }
 
-
+        });
 
 
     }
--- app/src/main/java/com/e/findid/result.java
+++ app/src/main/java/com/e/findid/result.java
@@ -4,18 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class idresult extends AppCompatActivity {
+public class result extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_idresult);
+        setContentView(R.layout.activity_result);
 
         //액션바
-
-
-
-
-
+        //getSupportActionBar().setTitle("원더패스"); //액션바 제목
     }
 }
